f86bb2b1bcb3c68460be2fb14c8bb63009923783
ShadowGreg <shadow-greg@yandex.ru>
2022-12-11 19:24:10 +0300

    поправил функции поиска телефона - она теперь возвращет массив элементов

--- funcs.py
+++ funcs.py
@@ -1,7 +1,9 @@
 # TOTO add next functions
 # search
 
-def search_phone(input_phone_number: str, input_array: list):
+def search_phone(input_phone_number: str, input_array: list) -> list:
+    output_records = []
     for record_line in input_array:
         if input_phone_number in record_line[2]:
-            return record_line
+            output_records.append(record_line)
+    return output_records
