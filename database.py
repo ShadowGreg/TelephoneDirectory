7f2720f9b99e35fe40796a553a7a78ff398c639d
Andrew <>
2022-12-14 01:11:39 +0700

    a

--- database.py
+++ database.py
@@ -1,21 +1,29 @@
+
 def write_to_csv(input_data: list, input_file_name: str = 'bp.csv'):
     with open(input_file_name, "a", encoding='utf-8') as data:
         data.write(f'{input_data[0]};{input_data[1]};{input_data[2]};{input_data[3]}\n')
 
 
-def write_to_txt(input_data: list, input_file_name: str = 'bp.txt'):
+def write_to_txt(input_data: list [str], input_file_name: str = 'bp.txt'):
     with open(input_file_name, "a", encoding='utf-8') as data:
         data.write(
-            f'Фамилия: {input_data[0]}'
-            f'\n\nИмя: {input_data[1]}'
-            f'\n\nНомер телефона: {input_data[2]}'
-            f'\n\nОписание: {input_data[3]} \n\n\n')
+            f'{input_data[0]}'
+            f'\n{input_data[1]}'
+            f'\n{input_data[2]}'
+            f'\n{input_data[3]}\n\n\n')
 
-
-def read_from_file(input_file_name: str = 'bp.csv'):
+def read_from_file_txt(input_file_name: str = 'bp.txt'):
     with open(input_file_name, "r", encoding='utf-8') as data:
         file_data = data.read()
+        file_data = file_data.replace('-', '')
+        file_data = file_data.replace('\n', ' ')
+        file_data = file_data.split('   ')
+        lst = []
+        for word in file_data:
+            s = word.split(' ')
+            lst.append(s)     
+        print(lst)
     return file_data
 
-
+read_from_file_txt()
 # TODO add delete
