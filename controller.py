4f0b5d2bf5592c64fd2efc289cad2ae581420aaf
ShadowGreg <shadow-greg@yandex.ru>
2022-12-15 10:59:18 +0300

    Обновил контроллер - для красоты

--- controller.py
+++ controller.py
@@ -8,16 +8,11 @@ def choose_an_action(case_flag=True):
     while case_flag:
         choice = view.input_choose()
         if choice == 1:
-            phone_data = funcs.search_by_phone(
-                view.input_phone_number(),
-                funcs.get_data_from_bd(database.read_from_file())
-            )
-            for item in phone_data:
-                view.print_data(item)
+            phone_data = search_by_input_data()
+            print_array_data_from(phone_data)
         if choice == 2:
             data_base = funcs.get_data_from_bd(database.read_from_file())
-            for item in data_base:
-                view.print_data(item)
+            print_array_data_from(data_base)
         if choice == 3:
             new_line_data = funcs.add_new_line_in_bd(view.input_data())
             database.write_to_csv(new_line_data)
@@ -30,3 +25,14 @@ def choose_an_action(case_flag=True):
             view.print_choose_action_menu()
         if choice == 6:
             case_flag = False
+
+
+def print_array_data_from(input_data: list[list[str]]):
+    for item in input_data:
+        view.print_data(item)
+
+
+def search_by_input_data() -> list[list[str]]:
+    return funcs.search_by_phone(
+        view.input_phone_number(),
+        funcs.get_data_from_bd(database.read_from_file()))
