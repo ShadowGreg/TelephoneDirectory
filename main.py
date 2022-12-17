43b30594e3e3b9d502206f35fda708dba119e15a
Andrew <>
2022-12-17 12:15:31 +0700

    Final changes

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import controller
 
-controller.choose_an_action()
+# controller.choose_an_action()
 
+def main():
+    controller.choose_an_action()
+   
+if __name__ == '__main__':
+        main()
