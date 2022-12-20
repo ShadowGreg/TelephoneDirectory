2d543b4a3e9a2e728034661715e5464e0c40a406
Andrew Choi <a.choi@smartsoltech.kr>
2022-12-20 21:42:14 +0900

    Мелкие правки

--- exception.py
+++ exception.py
@@ -1,6 +1,9 @@
 import logging
+import os
 
-logging.basicConfig(filename='app.log',
+log_dir = os.path.dirname(__file__) + '/log_dir/'
+
+logging.basicConfig(filename=log_dir + 'app.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s [%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
