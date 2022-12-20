39ef5a0908cb797a1d9c1bfb5928beb5b1532625
Andrew Choi <a.choi@smartsoltech.kr>
2022-12-20 19:25:05 +0900

    in it

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from exception import logging
 import os
+import logger_file
 
 
 def write_to_csv(input_data: list[str], input_file_name: str = 'bp.csv'):
     try:
         with open(input_file_name, "a", encoding='utf-8') as data:
             data.write(f'{input_data[0]};{input_data[1]};{input_data[2]};{input_data[3]}\n')
+            logger_file.log_info(f'Record {data} was written to CSV file', 'write_to_text')
     except Exception as e:
         logging.debug(e)
 
@@ -18,6 +20,7 @@ def write_to_txt(input_data: list[str], input_file_name: str = 'bp.txt'):
                 f'\n{input_data[1]}'
                 f'\n{input_data[2]}'
                 f'\n{input_data[3]}\n\n')
+            logger_file.log_info(f'Record {data} was written to text file', 'write_to_text')
     except Exception as e:
         logging.debug(e)
 
@@ -32,6 +35,7 @@ def read_from_file(input_file_name: str = 'bp.csv') -> str:
             with open(input_file_name, "r", encoding='utf-8') as data:
                 file_data = data.read()
                 return file_data
+            logger_file.log_info(f'Record {file_data} was requested from database', 'read_from_file')
     except Exception as e:
         logging.debug(e)
 
--- logger_file.py
+++ logger_file.py
@@ -5,9 +5,16 @@ import yaml
 import os
 
 log_dir = './log_dir/'
-with open('registr_info.yaml', 'r') as f:
-    comment = yaml.safe_load(f)
-    #print(comment['descr']['search']['desciption'])
+root_dir = './'
+try:
+    with open(root_dir+'reg_info.yaml', 'r') as f:
+          comment = yaml.safe_load(f)
+except yaml.YAMLError as e:
+    print('Error while opening YAML file' + str(e))
+
+
+    
+print(comment['descr']['search']['desciption'])
 def registretion_info():
     pass
 
@@ -15,10 +22,10 @@ def log_info(st: str, fname ):
     log_date = datetime.datetime.now().strftime("%Y-%m-%d")
     log_time = datetime.datetime.now().strftime("%H_%M")
     try:
-        with open(log_dir+'log_'+log_date+'.log', 'a') as f:
+        with open(log_dir+'log_'+log_date+'.log', 'rw') as f:
              f.write(f'{log_date}:{log_time} :[{fname}] : {st}\n')
     except Exception as e:
         logging.debug(e)
  
     
-#log_info('',  comment['descr']['add_contact']['desciption'])
+log_info('',  comment['descr']['add_contact']['desciption'])
